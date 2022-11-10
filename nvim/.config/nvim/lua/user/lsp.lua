429014b6f187d85ee0565944f3e415dfa0f00963
Alessio Andronico <alessio.andronico@gmail.com>
2022-11-10 06:40:00 +0100

    Fix LSP deprecation warning

--- nvim/.config/nvim/lua/user/lsp.lua
+++ nvim/.config/nvim/lua/user/lsp.lua
@@ -82,7 +82,7 @@ local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
+capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
