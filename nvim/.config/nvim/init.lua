358da707584f709b9108804cef42d80154cff176
Alessio Andronico <alessio.andronico@gmail.com>
2022-11-11 16:53:52 +0100

    Disable null-ls

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,15 +1,14 @@
-require "user.plugins-setup"
-require "user.core.colorscheme"
-require "user.core.options"
-require "user.core.keymaps"
-require "user.plugins.nvim-cmp"
-require "user.plugins.telescope"
-require "user.plugins.treesitter"
-require "user.plugins.comment"
-require "user.plugins.gitsigns"
-require "user.plugins.lualine"
-require "user.plugins.lsp.mason"
-require "user.plugins.lsp.lspsaga"
-require "user.plugins.lsp.lspconfig"
-require "user.plugins.lsp.null-ls"
-
+require("user.plugins-setup")
+require("user.core.colorscheme")
+require("user.core.options")
+require("user.core.keymaps")
+require("user.plugins.nvim-cmp")
+require("user.plugins.telescope")
+require("user.plugins.treesitter")
+require("user.plugins.comment")
+require("user.plugins.gitsigns")
+require("user.plugins.lualine")
+require("user.plugins.lsp.mason")
+require("user.plugins.lsp.lspsaga")
+require("user.plugins.lsp.lspconfig")
+-- require "user.plugins.lsp.null-ls"
