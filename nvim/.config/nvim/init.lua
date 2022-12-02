9e6faed0184dc440ee39feeaca41572319760eca
Alessio Andronico <alessio.andronico@gmail.com>
2022-12-02 07:37:39 +0100

    Add basic Conjure config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -13,3 +13,4 @@ require("user.plugins.lsp.mason")
 require("user.plugins.lsp.lspsaga")
 require("user.plugins.lsp.lspconfig")
 -- require "user.plugins.lsp.null-ls"
+require("user.plugins.conjure")
