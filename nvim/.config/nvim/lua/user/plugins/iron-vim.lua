144eacb1c53db72aa227e09f259a0570ce4b1936
Alessio Andronico <alessio.andronico@gmail.com>
2022-12-09 09:09:40 +0100

    Use localleader

--- nvim/.config/nvim/lua/user/plugins/iron-vim.lua
+++ nvim/.config/nvim/lua/user/plugins/iron-vim.lua
@@ -20,18 +20,18 @@ iron.setup {
     repl_open_cmd = 'belowright 15 split',
   },
   keymaps = {
-    send_motion = "<space>sc",
-    visual_send = "<space>sc",
-    send_file = "<space>sf",
-    send_line = "<space>sl",
-    send_mark = "<space>sm",
-    mark_motion = "<space>mc",
-    mark_visual = "<space>mc",
-    remove_mark = "<space>md",
-    cr = "<space>s<cr>",
-    interrupt = "<space>s<space>",
-    exit = "<space>sq",
-    clear = "<space>cl",
+    send_motion = "<localleader>sc",
+    visual_send = "<localleader>sc",
+    send_file = "<localleader>sf",
+    send_line = "<localleader>sl",
+    send_mark = "<localleader>sm",
+    mark_motion = "<localleader>mc",
+    mark_visual = "<localleader>mc",
+    remove_mark = "<localleader>md",
+    cr = "<localleader>s<cr>",
+    interrupt = "<localleader>s<localleader>",
+    exit = "<localleader>sq",
+    clear = "<localleader>cl",
   },
   -- If the highlight is on, you can change how it looks
   -- For the available options, check nvim_set_hl
@@ -42,8 +42,8 @@ iron.setup {
 }
 
 -- iron also has a list of commands, see :h iron-commands for all available commands
-vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
-vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
-vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
-vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+vim.keymap.set('n', '<localleader>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<localleader>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<localleader>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<localleader>rh', '<cmd>IronHide<cr>')
 
