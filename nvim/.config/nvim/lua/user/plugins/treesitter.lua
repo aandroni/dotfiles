f14bbb2518161241bc7fe4fe005fc3c443bc6428
Alessio Andronico <alessio.andronico@gmail.com>
2023-04-29 17:13:17 +0200

    Ensure installed only a few languages

--- nvim/.config/nvim/lua/user/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/user/plugins/treesitter.lua
@@ -4,7 +4,31 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {
+    "bash",
+    "c",
+    "clojure",
+    "cpp",
+    "css",
+    "go",
+    "html",
+    "java",
+    "javascript",
+    "json",
+    "lua",
+    "luadoc",
+    "luap",
+    "markdown",
+    "python",
+    "query",
+    "r",
+    "regex",
+    "toml",
+    "typescript",
+    "vim",
+    "vimdoc",
+    "yaml",
+  },
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
