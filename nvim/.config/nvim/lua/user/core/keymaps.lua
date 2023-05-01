f6f1e76a182e8d28acec94e4f3e396954922921e
Alessio Andronico <alessio.andronico@gmail.com>
2023-05-01 20:52:04 +0200

    Switch to lazy.nvim

--- nvim/.config/nvim/lua/user/core/keymaps.lua
+++ nvim/.config/nvim/lua/user/core/keymaps.lua
@@ -5,10 +5,10 @@ local opts = { noremap = true, silent = true }
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
---Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
+-- --Remap space as leader key
+-- keymap("", "<Space>", "<Nop>", opts)
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = ","
 
 -- Modes
 --   normal_mode       = "n",
--- nvim/.config/nvim/lua/user/plugins-setup.lua
+++ nvim/.config/nvim/lua/user/plugins-setup.lua
@@ -1,127 +1,97 @@
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system {
-      "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path,
-    }
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
 
-local packer_bootstrap = ensure_packer()
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-  return
-end
-
--- Have packer use a popup window
-packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-}
+--Remap space as leader key
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
 
 -- Plugins
-return packer.startup(function(use)
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-
+require('lazy').setup({
   -- popup and plenary are required by many other plugins
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+  "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
 
   -- window management
   -- use "christoomey/vim-tmux-navigator"
-  use "szw/vim-maximizer"
+  "szw/vim-maximizer",
 
   -- file explorer
-  use "nvim-tree/nvim-tree.lua"
-  use "kyazdani42/nvim-web-devicons"
+  "nvim-tree/nvim-tree.lua",
+  "kyazdani42/nvim-web-devicons",
 
   -- Essential plugins
-  use "tpope/vim-surround"
-  use "numToStr/Comment.nvim"
+  "tpope/vim-surround",
+  "numToStr/Comment.nvim",
 
   -- Colorschemes
-  use "folke/tokyonight.nvim"
-  use "gruvbox-community/gruvbox"
-  use "arcticicestudio/nord-vim"
-  use { "catppuccin/nvim", as = "catppuccin" }
+  "folke/tokyonight.nvim",
+  "gruvbox-community/gruvbox",
+  "arcticicestudio/nord-vim",
+  { "catppuccin/nvim", name = "catppuccin" },
 
   -- Statusline
-  use "nvim-lualine/lualine.nvim"
+  "nvim-lualine/lualine.nvim",
 
   -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp" -- LSP
-  use "hrsh7th/cmp-nvim-lua" -- Neovim API
+  "hrsh7th/nvim-cmp", -- The completion plugin
+  "hrsh7th/cmp-buffer", -- buffer completions
+  "hrsh7th/cmp-path", -- path completions
+  "hrsh7th/cmp-cmdline", -- cmdline completions
+  "saadparwaiz1/cmp_luasnip", -- snippet completions
+  "hrsh7th/cmp-nvim-lsp", -- LSP
+  "hrsh7th/cmp-nvim-lua", -- Neovim API
 
   -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  "L3MON4D3/LuaSnip", --snippet engine
+  "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
   -- LSP
-  use "neovim/nvim-lspconfig"
-  use "williamboman/mason.nvim"
-  use "williamboman/mason-lspconfig.nvim"
-  use { "glepnir/lspsaga.nvim", branch = "main" }
-  use "onsails/lspkind.nvim"
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  { "glepnir/lspsaga.nvim", branch = "main" },
+  "onsails/lspkind.nvim",
 
   -- formatting and linting
-  use "jose-elias-alvarez/null-ls.nvim"
-  use "jayp0521/mason-null-ls.nvim"
+  "jose-elias-alvarez/null-ls.nvim",
+  "jayp0521/mason-null-ls.nvim",
 
   -- Telescope
-  use { "nvim-telescope/telescope-fzf-native.nvim", run = "make" }
-  use { "nvim-telescope/telescope.nvim", branch = "0.1.x" }
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
 
   -- Treesitter (run update automatically after install)
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  }
+  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
   -- Git
-  use "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
 
   -- Clojure
-  use "tpope/vim-dispatch"
-  use "clojure-vim/vim-jack-in"
-  -- use "tpope/vim-fireplace"
-  use "Olical/conjure"
+  "tpope/vim-dispatch",
+  "clojure-vim/vim-jack-in",
+  -- "tpope/vim-fireplace",
+  "Olical/conjure",
 
   -- Send text to screen/tmux/kitty
-  use "jpalardy/vim-slime" -- works but cannot send to qtconsole
-  use "mtikekar/nvim-send-to-term" -- works with qt console (but a little weird)
-  use "HiPhish/repl.nvim" -- Cannot send lines
-  use "bfredl/nvim-ipy" -- Doesn't work with qtconsole
-  use "hkupty/iron.nvim"
+  "jpalardy/vim-slime", -- works but cannot send to qtconsole
+  "mtikekar/nvim-send-to-term", -- works with qt console (but a little weird)
+  "HiPhish/repl.nvim", -- Cannot send lines
+  "bfredl/nvim-ipy", -- Doesn't work with qtconsole
+  "hkupty/iron.nvim",
 
   -- Go
-  use "fatih/vim-go"
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require("packer").sync()
-  end
-end)
+  "fatih/vim-go",
+})
 
