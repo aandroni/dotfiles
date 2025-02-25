44751a1228ed1e90677ad8dc207aaa13943759ac
Alessio Andronico <alessio.andronico@gmail.com>
2025-02-25 07:18:35 +0100

    Change color theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -803,7 +803,9 @@ require("lazy").setup({
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		-- "folke/tokyonight.nvim",
-		"folke/tokyonight.nvim",
+		-- "folke/tokyonight.nvim",
+		"projekt0n/github-nvim-theme",
+    name = "github-theme",
     lazy = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		init = function()
@@ -811,7 +813,8 @@ require("lazy").setup({
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
 			-- vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd.colorscheme("tokyonight-storm")
+			-- vim.cmd.colorscheme("tokyonight-storm")
+      vim.cmd('colorscheme github_dark')
 
 			-- You can configure highlights by doing something like:
 			vim.cmd.hi("Comment gui=none")
